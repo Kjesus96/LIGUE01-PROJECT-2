f4ae66890759c7144c78e60043cb5e57d0b88da7
Keylor Cruz Carvajal <Kjesus96@gmail.com>
2018-05-10 00:16:51 -0600

    carperta SQL
    contiene los .sql para base de datos mysql

---
+++ SQL/equipos.sql
@@ -0,0 +1,21 @@
+insert into equipos(Id,Nombre,Latitud,Longitud) values("ASC","Amiens SC",49.894209,2.263312);
+insert into equipos(Id,Nombre,Latitud,Longitud) values("ACO","Angers SCO",47.460458,-0.530741);
+insert into equipos(Id,Nombre,Latitud,Longitud) values("ASM","AS Monaco",43.727778,7.415556);
+insert into equipos(Id,Nombre,Latitud,Longitud) values("ASE","AS Saint Etienne",45.460833,4.39);
+insert into equipos(Id,Nombre,Latitud,Longitud) values("DFC","Dijon FCO",47.324469,5.069247);
+insert into equipos(Id,Nombre,Latitud,Longitud) values("EAG","EA Guingamp",48.566286,-3.164597);
+insert into equipos(Id,Nombre,Latitud,Longitud) values("EST","ESTAC Troyes",48.307561,4.098458);
+insert into equipos(Id,Nombre,Latitud,Longitud) values("FCM","FC Metz",49.109813, 6.159511);
+insert into equipos(Id,Nombre,Latitud,Longitud) values("FCN","FC Nantes",47.255556,-1.525278);
+insert into equipos(Id,Nombre,Latitud,Longitud) values("FCG","Girondins Bordeaux",44.896944,-0.563333);
+insert into equipos(Id,Nombre,Latitud,Longitud) values("LSP","Lille OSC",50.611944,3.130556);
+insert into equipos(Id,Nombre,Latitud,Longitud) values("MSP","Montpellier HSC",43.621944,3.811944);
+insert into equipos(Id,Nombre,Latitud,Longitud) values("OGN","OGC Nice",43.706944,7.194444);
+insert into equipos(Id,Nombre,Latitud,Longitud) values("OLL","Olympique Lyon",45.765224,4.982131);
+insert into equipos(Id,Nombre,Latitud,Longitud) values("OLM","Olympique Marseille",43.269722,5.395833);
+insert into equipos(Id,Nombre,Latitud,Longitud) values("PSG","Paris Saint Germain",48.841389,2.253056);
+insert into equipos(Id,Nombre,Latitud,Longitud) values("RCS","RC Strasbourg",48.560064, 7.754969);
+insert into equipos(Id,Nombre,Latitud,Longitud) values("SMC","SM Caen",49.179444,-0.39675);
+insert into equipos(Id,Nombre,Latitud,Longitud) values("SRF","Stade Rennes",48.107444,-1.712833);
+insert into equipos(Id,Nombre,Latitud,Longitud) values("TFC","Toulouse FC",43.583296,1.434055);
+
